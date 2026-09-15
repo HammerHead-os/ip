cee5bef7284803fde6d04b18e27f9fc7ab747be7
HammerHead-os <gpenumatsa@connect.ust.hk>
2026-09-16 02:33:43 +0800

    Complete Level-2: Store items in array and support list command

--- src/main/java/Hedy.java
+++ src/main/java/Hedy.java
@@ -2,30 +2,33 @@ import java.util.Scanner;
 
 public class Hedy {
     public static void main(String[] args) {
-        String logo = " _    _          _       \n"
-                    + "| |  | |        | |      \n"
-                    + "| |__| |   ___  | | _  _ \n"
-                    + "|  __  |  / _ \\ | || |/ / \n"
-                    + "| |  | | |  __/ | | |   <  \n"
-                    + "|_|  |_|  \\___| |_| |_|\\_\\ \n";
-        System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Hedy.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String line;
 
         while (true) {
             line = scanner.nextLine().trim();
             System.out.println("____________________________________________________________");
+
             if (line.equalsIgnoreCase("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (line.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = line;
+                taskCount++;
+                System.out.println(" added: " + line);
             }
-            System.out.println(" " + line);
             System.out.println("____________________________________________________________");
         }
         scanner.close();
